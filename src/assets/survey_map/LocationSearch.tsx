8d5845ff80a7115bf98efdd1884875e3787604da
Anand Pandey <anand.pandey1052@gmail.com>
2024-08-12 11:39:24 +0530

    Fixed Prod

--- src/assets/survey_map/LocationSearch.tsx
+++ src/assets/survey_map/LocationSearch.tsx
@@ -1,33 +1,33 @@
-import React, { useEffect, useRef } from 'react';
+// import React, { useEffect, useRef } from 'react';
 
-const LocationSearch = ({ onSelectLocation }: { onSelectLocation: (coordinates: { lat: number; lng: number }) => void }) => {
-  const searchInputRef = useRef<HTMLInputElement>(null);
+// const LocationSearch = ({ onSelectLocation }: { onSelectLocation: (coordinates: { lat: number; lng: number }) => void }) => {
+//   const searchInputRef = useRef<HTMLInputElement>(null);
 
-  useEffect(() => {
-    if (typeof window !== 'undefined' && window.google) {
-      const autocomplete = new window.google.maps.places.Autocomplete(searchInputRef.current as HTMLInputElement);
+//   useEffect(() => {
+//     if (typeof window !== 'undefined' && window.google) {
+//       const autocomplete = new window.google.maps.places.Autocomplete(searchInputRef.current as HTMLInputElement);
       
-      autocomplete.addListener('place_changed', () => {
-        const place = autocomplete.getPlace();
-        if (place.geometry && place.geometry.location) {
-          const lat = place.geometry.location.lat();
-          const lng = place.geometry.location.lng();
-          onSelectLocation({ lat, lng });
-        }
-      });
-    }
-  }, []);
+//       autocomplete.addListener('place_changed', () => {
+//         const place = autocomplete.getPlace();
+//         if (place.geometry && place.geometry.location) {
+//           const lat = place.geometry.location.lat();
+//           const lng = place.geometry.location.lng();
+//           onSelectLocation({ lat, lng });
+//         }
+//       });
+//     }
+//   }, []);
 
-  return (
-    <div className="w-full">
-      <input
-        type="text"
-        ref={searchInputRef}
-        placeholder="Search for a place"
-        className="w-full p-2 border rounded"
-      />
-    </div>
-  );
-};
+//   return (
+//     <div className="w-full">
+//       <input
+//         type="text"
+//         ref={searchInputRef}
+//         placeholder="Search for a place"
+//         className="w-full p-2 border rounded"
+//       />
+//     </div>
+//   );
+// };
 
-export default LocationSearch;
+// export default LocationSearch;
